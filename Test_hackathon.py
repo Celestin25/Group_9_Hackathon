840988e9ae02dafcd810d9e4ffa1a481a5c53ff2
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-09-24 17:24:27 +0300

    group nine work

--- Test_hackathon.py
+++ Test_hackathon.py
@@ -1,1 +1,34 @@
 #Welcome to Test_hackathon.py
+import re
+ #for checking Email
+def emailChecker():
+
+   match = re.compile(r"'^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'") # Your Regex goes between the double quotes
+
+   email = input("Enter the Email: ")
+
+   if re.fullmatch(match, email):
+       print (f"'{email}'looks like a valid Email")
+   else:
+       print ("Invalid Email")
+#for checking Phone Number
+def check_form(data):
+
+   Pattern = re.compile("(0/91)?[6-9][0-9]{9}")
+
+   Phone_Number=input("Enter your phone number:")
+
+   if re.fullmatch(Pattern,Phone_Number):
+       print(f"'{Phone_Number}'looks like a valid phone number")
+   else:
+       print("invalid phone number")
+#for checking name
+Full_name=input("Enter your Full name")
+def check_form(data):
+    if data['Full_name'].isdigit():
+        print(f"'Full_name'looks invalid")
+
+    else:
+        print("full name is valid")
+
+#for checking Username
